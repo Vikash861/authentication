ac1d75737693d224dc3f8aca2df0acb7bbb64bec
vikash <kumarvikashv681@gmail.com>
2022-08-03 19:49:53 +0530

    session and cookie based authetication added

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const encrypt = require('mongoose-encryption');//for simple ceaser method encryption
-var md5 = require('md5');// hasing enryption
 const bcrypt = require('bcrypt');//salting and hashing
 const saltRounds = 2;
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+const session = require('express-session');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
+
 /////////////////////////////////////////////DATABASE////////////////////////////////////////////////
 mongoose.connect("mongodb://localhost:27017/usersDB")
 // to use encryption we have to define schema using class object///////
@@ -28,15 +38,40 @@ const user = mongoose.model('users', userSchema);
 
 ////////////////////////////////////Home/////////////////////////////////////////////////////
 app.get('/', (req, res) => {
-    res.render('home');
+    const session = req.session;
+    console.log(session.userid);
+    if (session.userid) {
+        res.render('secrets')
+    }
+    else
+        res.render('home');
 })
 //////////////////////////////////////////Login////////////////////////////////////////////////////
 app.get('/login', (req, res) => {
-    res.render('login');
+    const session = req.session;
+    console.log(session.userid);
+    if (session.userid) {
+        res.render('secrets')
+    }
+    else{
+        res.render('login');
+    }
 })
 //////////////////////////////////////////Register////////////////////////////////////////
 app.get('/register', (req, res) => {
-    res.render('register');
+    const session = req.session;
+    console.log(session.userid);
+    if (session.userid) {
+        res.render('secrets')
+    }
+    else{
+        res.render('register');
+    }
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 })
 
 ////////////////////////////////////////////////////POST///////////////////////////////////////////
@@ -44,42 +79,36 @@ app.get('/register', (req, res) => {
 
 
 app.post('/register', (req, res) => {
-
-
-    user.findOne({ email: req.body.username })
-        .then((foundUser) => {
-            bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-                if (foundUser == null) {
-                    const userRegDetails = new user({
-                        email: req.body.username,
-                        password: hash
-                    })
-                    userRegDetails.save((err) => {
-                        if (err) return handleError(err)
-                        else
-                            res.render('secrets');
-                    })
-                }
-                else {
-                    res.render('secrets')
-                }
-            });
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const userRegDetails = new user({
+            email: req.body.username,
+            password: hash
         })
-        .catch(err => console.log(err));
 
 
+        userRegDetails.save((err) => {
+            if (err) return handleError(err)
+            else {
+                const session = req.session;
+                session.userid = req.body.username;
+                res.render('secrets');
+            }
+        })
+    });
 })
 
 app.post('/login', (req, res) => {
 
     user.findOne({ email: req.body.username })
         .then((foundUser) => {
-            if(foundUser != null){
-                bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
-                    if(result === true){
+            if (foundUser != null) {
+                bcrypt.compare(req.body.password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        const session = req.session;
+                        session.userid = req.body.username;
                         res.render('secrets');
                     }
-                    else{
+                    else {
                         res.render("wrong password");
                     }
                 });
